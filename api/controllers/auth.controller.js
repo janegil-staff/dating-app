138fc7a24aadd2e5a90623cc37c8b7df4bc04d3a
Jan Egil Staff Øvretveit <janstovr@Mac.lan>
2025-07-02 15:58:02 +0200

    Add login route in backend

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
-import crypto from "crypto";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 import User from "../models/User.js";
 import { sendVerificationEmail } from "../utils/sendVertificationEmail.js";
@@ -36,5 +37,31 @@ export const register = async (req, res, next) => {
   } catch (error) {
     console.log("Error registering user", error);
     res.status(500).json({ message: "Registration failed" });
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    //check if the user exists already
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    //check in password is correct
+    let passwordMatches = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatches) {
+      return res.status(401).json({ message: "Invalide password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "login failed" });
   }
 };
